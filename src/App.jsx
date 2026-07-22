d24e485c0c9ef26cf60fe2538588eedd6381d57a
Daniel Edjang <danieledjang1@gmail.com>
2026-07-22 13:18:58 +0100

    settings

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ForgotPassword from "./pages/ForgotPassword";
 import Settings from "./settings/pages/settings";
+import SettingsPage from "./pages/settingsPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/signup" element={<Signup />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/settings" element={<Settings />} />
+      <Route path="/settingsPage" element={<SettingsPage />} />
       {/* <Route path="/dashboard" element={<Dashboard />} /> */}
     </Routes>
   );
