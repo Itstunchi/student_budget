0b9f877a05e2dd650c7656c6f0e5f5d22fa56a98
Achiever0001 <leeldha47@gmail.com>
2026-07-22 10:11:10 +0100

    feat:Updated BUddy AI

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ForgotPassword from "./pages/ForgotPassword";
+import BudgetAdvisor from "./components/BudgetAdvisor";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/advisor" element={<BudgetAdvisor />} />
       {/* <Route path="/dashboard" element={<Dashboard />} /> */}
     </Routes>
   );
