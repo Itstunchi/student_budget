65eed3ea605e198992faa07797c53989f5c6a9b7
chybejoseph-ctrl <chybejoseph69@gmail.com>
2026-07-24 13:49:47 +0100

    Restore project files after sidebar merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
 
-
-
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Sidebar/>} />
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
