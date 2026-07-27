85ac4f828c4876188cd2451c1e51ecabedef6c9e
Achiever0001 <leeldha47@gmail.com>
2026-07-27 10:40:04 +0100

    feat:my latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Signup from "./pages/Signup";
 import ForgotPassword from "./pages/ForgotPassword";
 import BudgetAdvisor from "./components/BudgetAdvisor";
 import Calendar from "./components/Calendar";
+import SpendingPlanWizard from "./components/SpendingPlanWizard";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/advisor" element={<BudgetAdvisor />} />
       <Route path="/calendar" element={<Calendar />} />
+      <Route path="/spending-plan" element={<SpendingPlanWizard />} />
       {/* <Route path="/dashboard" element={<Dashboard />} /> */}
     </Routes>
   );
